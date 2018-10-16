ffda916ea9027811394cf7eca5661322ccad1470
Tdilotsotlhe <phenom92@gmail.com>
2018-10-16 14:29:17 -0700

    setup fixed options

--- config/database.php
+++ config/database.php
@@ -4,7 +4,7 @@ $DB_USER = "root";
 $DB_PASS = "adambogas123";
 $DB_NAME = "camagru";
 $options = [
-    PDO::ATTR_EMULATE_PREPARES   => false, // turn off emulation mode for "real" prepared statements
+    // turn off emulation mode for "real" prepared statements
     PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION, //turn on errors in the form of exceptions
     PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC, //make the default fetch be an associative array
   ];
